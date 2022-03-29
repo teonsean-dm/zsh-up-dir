70625b71e502b8fbc6e2ff39e3dcaaabc147a412
Samuel Thomas <sgpthomas@gmail.com>
2022-03-29 10:27:27 -0500

    first commit

---
+++ up-dir.plugin.zsh
@@ -0,0 +1,20 @@
+PROG='@a = split " "; 
+print join " ", @a[0..($#a-1)]; 
+if (rindex($a[$#a], "/") != -1) {
+  if (scalar @a > 1) { print " "; }
+  @b = split "/", $a[$#a];
+  print join "/", @b[0..($#b-1)]
+}'
+
+function _up-dir {
+    if [ -z $BUFFER ]; then
+        parent="$(dirname $(pwd))"
+        cd $parent
+        zle reset-prompt
+    else
+        BUFFER=$(echo $BUFFER | perl -ne $PROG)
+    fi
+
+}
+zle -N _up-dir
+bindkey "^h" _up-dir
