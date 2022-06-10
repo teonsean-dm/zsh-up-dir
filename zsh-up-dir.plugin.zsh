4b518756231119efca5b7dcce4ad4a7aab41b32d
MinjieFang <minjiefang@deepmirror.com>
2022-06-10 18:09:03 +0800

    Update zsh-up-dir.plugin.zsh

--- zsh-up-dir.plugin.zsh
+++ zsh-up-dir.plugin.zsh
@@ -10,7 +10,7 @@ function _up-dir {
     if [ -z $BUFFER ]; then
         parent="$(dirname $(pwd))"
         cd $parent
-        zle reset-prompt
+        p10k display -r
     else
         BUFFER=$(echo $BUFFER | perl -ne $PROG)
     fi
